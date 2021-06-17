6d4995b3b531cc5434f1732218524276785a14d2
Kiel Sprague <aereisdin@gmail.com>
2021-06-17 12:23:03 -0700

    Modifying the folder structure

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './App.css';
-import Nav from "./components/Nav";
-import Foot from "./components/Foot";
-import Hero from "./components/Hero";
-// import Contact from "./pages/Contact";
-import AboutMe from "./pages/AboutMe";
-import Projects from "./pages/Projects";
-import Goals from "./pages/Goals";
+import Navigation from './components/Nav/Navigation';
+import './App.scss';
+import Footer from './components/Nav/Footer';
 
 function App() {
-
-
-
   return (
-    
-    <Router>
-      <div>
-      <Nav />
-      <Switch>
-      <Route exact path="/">
-        <Hero/>
-      </Route>
-      <Route exact path="/projects">
-        <Projects/>
-      </Route>
-      <Route exact path="/goals">
-        <Goals/>
-      </Route>
-      {/* <Route exact path="/contact" component={Contact} /> */}
-      <Route exact path="/whoami">
-        <AboutMe/>
-      </Route>
-      </Switch>
-      <Foot />
-      </div>
-    </Router>
-    
+    <>
+    <Navigation/>
+    <Footer/>
+    </>
   );
 }
 
---
+++ src/components/Nav/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Row, Col, Container } from 'react-bootstrap';
+function Footer() {
+    return (
+    <Container fluid>    
+       <Row className="footer">
+           <Col>
+           linkedin link
+           </Col>
+           <Col>
+           Kiel Sprague ~ 2021
+           </Col>
+           <Col>
+           aereisdin@gmail.com
+           </Col>
+       </Row>
+    </Container>
+    )
+}
+
+export default Footer
