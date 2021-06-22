187db444e4a5e954e89ff6f70915ccf3a878c863
Kiel Sprague <aereisdin@gmail.com>
2021-06-22 12:21:08 -0700

    carousel work

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,55 +7,66 @@ function Body() {
     return (
         <Container>
             <Row style={{height: "50px"}}>
-                <Col>
-                    These are the words you see.
-                </Col>
+              
             </Row>
             <Row className="d-flex justify-content-evenly">
                 <Col sm={10} md={10} lg={10}>
             <Carousel>
                 <Carousel.Item>
                     <img
+                    style={{borderRadius: "5px"}}
                     className="d-block c-img"
                     src="kiel.JPG"
                     alt="First slide"
                     />
                     <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <h3 style={{fontWeight: "700"}}>Kiel Sprague</h3>
+                    <p>Dreaming big!</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
+                    style={{borderRadius: "5px"}}
                     className="d-block c-img"
                     src="plantsy.png"
                     alt="Second slide"
                     />
 
                     <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h3 style={{fontWeight: "700"}}>Plantsy</h3>
+                    <p>A React Project</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
+                    style={{borderRadius: "5px"}}
+                    className="d-block c-img"
+                    src="jnk.jpg"
+                    alt="First slide"
+                    />
+                    <Carousel.Caption>
+                    <h3 style={{fontWeight: "700"}}>Jonathan & Kiel</h3>
+                    <p>Taking time to enjoy nature.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    style={{borderRadius: "5px"}}
                     className="d-block c-img"
                     src="screenshot.png"
                     alt="Third slide"
                     />
 
                     <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h3 style={{fontWeight: "700"}}>My first page</h3>
+                    <p>My very first published project.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
             </Col>  
             </Row>
             <Row>
-                <Col>
-                    So exciting!!!
-                </Col>
+             
             </Row>
         </Container>
     )
