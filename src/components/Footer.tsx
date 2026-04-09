23126f60e15c9a6b60d848b7c2a66efced79fe6b
Kiel Sprague <kiel@adventuresmithexplorations.com>
2026-04-09 15:37:04 -0700

    Added icons for Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,41 @@
+import GitHubIcon from '../assets/github.svg?react'
+import InstagramIcon from '../assets/instagram.svg?react'
+import LinkedInIcon from '../assets/linkedin.svg?react'
+import './Footer.css'
+
 function Footer() {
   return (
-    <footer>
-      <h2>Building a better tomorrow</h2>
-      <p>Footer | Stuff | Goes Here</p>
+    <footer className="footer">
+      <h2>Building a Better Tomorrow</h2>
+      <p>
+      <a
+        href="https://linkedin.com/in/kiel-sprague"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Visit my LinkedIn profile (opens in a new tab)"
+      >
+       <LinkedInIcon className="icon" />
+      </a> |&nbsp;
+      <a
+        href="https://www.instagram.com/aereisdin/"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Visit my Instagram profile (opens in a new tab)"
+      >
+        <InstagramIcon className="icon" />  
+      </a> |&nbsp;
+      <a
+        href="https://github.com/aereisdin"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Visit my GitHub profile (opens in a new tab)"
+      >
+        <GitHubIcon className="icon" />
+        </a></p>
+      <br/>
+      <p>&copy; 2024 Kiel Sprague. All rights reserved.
+        Photo by <a href="https://www.pexels.com/@itsehsanh/" target="_blank" rel="noopener noreferrer">Ehsan Haquez</a> on <a href="https://www.pexels.com/" target="_blank" rel="noopener noreferrer">Pexels</a> and <a href="https://www.instagram.com/itsehsanh" target="_blank" rel="noopener noreferrer">Instagram</a>.
+      </p>
     </footer>
   )
 }
